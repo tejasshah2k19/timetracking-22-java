458d41712cac81e98f114a3304a6c52bc233f51f
Tejas <tejas14all@gmail.com>
2022-02-09 08:58:48 +0530

    init

---
+++ src/main/java/com/TimetrackingApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetrackingApplication.class, args);
+	}
+
+}
