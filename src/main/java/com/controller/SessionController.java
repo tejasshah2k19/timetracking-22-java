bdc1604ae1a570445bbb92c239f3edbc460316a4
Tejas <tejas14all@gmail.com>
2022-02-11 08:59:09 +0530

    jsp configuration

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -1,5 +1,15 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class SessionController {
 
+	@RequestMapping(value = "signup",method = RequestMethod.GET)
+	public String signup() {
+		System.out.println("i am from signup....");
+		return "Signup";// this is your view page --> html
+	}
 }
